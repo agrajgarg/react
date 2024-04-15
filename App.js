32df8efce2b349550fada6309afff2e252d8a7e1
[Agraj Garg] <[agraj50@gmail.com]>
2024-04-15 16:49:32 +0530

    updated code from ep-03

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-  //JSX
-  
-  console.log(parent); // object
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+// const Title = ( <h1 id="heading" className="head">Namaste React using JSX</h1>);
+
+// const Title2 = () => {
+//     return <h1 id="heading" className="head">Namaste React using JSX2</h1>;
+// };
+
+// //Component Composition
+// const HeadingComponent = () => (
+//     <div id="container">
+        
+//         {Title2()}
+//         <h1>Functional Component in React</h1>
+//     </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+// root.render(<HeadingComponent/>);
+
+//coding assignment 3
+
+// const header = React.createElement("div", {class: "title"}, 
+// React.createElement("h1", {}, 
+// React.createElement("h2", {}, 
+// React.createElement("h3", {}, "i am h3"))));
+
+// const header = (
+//     <div className="title">
+//         <h1>
+//             <h2>
+//                 <h3>I am h3</h3>
+//             </h2>
+//         </h1>
+//     </div>
+// );
+
+// const H3 = () => (
+//     <h3>I am h3</h3>
+// );
+
+// const Header = () => (
+//     <div className="title">
+//         <h1>
+//             <h2>
+//                 <H3 />
+//             </h2>
+//         </h1>
+//     </div>
+// );
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Header />);
